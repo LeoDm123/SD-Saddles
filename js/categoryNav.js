89da4616ca97e91184cf9189087f0c7075bf6cdb
LeoDm123 <leo.meiners1@gmail.com>
2024-05-20 14:33:20 -0300

    filter improvemnt

--- js/categoryNav.js
+++ js/categoryNav.js
@@ -1,23 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
-  function getUrlParameter(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
-    var results = regex.exec(location.search);
-    return results === null
-      ? ""
-      : decodeURIComponent(results[1].replace(/\+/g, " "));
-  }
+window.addEventListener("load", function () {
+  const currentPage = window.location.pathname;
 
-  var categoryParam = getUrlParameter("category");
+  if (currentPage === "/" || currentPage === "/index.html") {
+    document.querySelectorAll(".cat-img").forEach((image) => {
+      image.addEventListener("click", function () {
+        const category = this.getAttribute("data-category");
+        localStorage.setItem("selectedCategory", category);
+        window.location.href = "/products.html";
+      });
+    });
+  } else if (currentPage === "/products.html") {
+    const selectedCategory = localStorage.getItem("selectedCategory");
+    if (selectedCategory) {
+      const filter = document.querySelector(`#${selectedCategory}`);
+      if (filter) {
+        filter.checked = true;
+        filter.dispatchEvent(new Event("change"));
+        window.addEventListener("load", filterProducts);
+      }
+      localStorage.removeItem("selectedCategory");
+    }
 
-  if (categoryParam) {
-    var checkbox = document.getElementById(
-      "checkbox" +
-        categoryParam.charAt(0).toUpperCase() +
-        categoryParam.slice(1)
-    );
-    if (checkbox) {
-      checkbox.checked = true;
+    document.querySelectorAll(".custom-checkbox input").forEach((checkbox) => {
+      checkbox.addEventListener("change", filterProducts);
+    });
+
+    function filterProducts() {
+      const checkboxes = document.querySelectorAll(".custom-checkbox input");
+      const cards = document.querySelectorAll(".product-card");
+      const selectedCategories = Array.from(checkboxes)
+        .filter((cb) => cb.checked)
+        .map((cb) => cb.parentElement.nextElementSibling.textContent.trim());
+
+      cards.forEach(function (card) {
+        const cardCategory = card.getAttribute("data-category");
+        const shouldShow =
+          selectedCategories.includes(cardCategory) ||
+          selectedCategories.length === 0;
+        card.style.display = shouldShow ? "block" : "none";
+      });
     }
   }
 });
